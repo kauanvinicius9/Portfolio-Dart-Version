4b7603a738915060a81aa00587a18e70240d3e4d
KAUAN VINÍCIUS SALES SILVA FRANCISCHINI <kauan.francischini@senaisp.edu.br>
2026-03-23 08:48:48 -0300

    starting

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Avaliacao());
+}
+
+class Avaliacao extends StatelessWidget {
+  const Avaliacao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Portfolio",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatelessWidget {
+  const Inicio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      backgroundColor: const Color(0xFFF5F6FA),
+      appBar: AppBar(
+        title: Text("Portfolio", style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+            ]
+          )
+      )
+    )
+  }
+ }
+
