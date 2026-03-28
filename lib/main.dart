cfaeda7fb041b306aa86ca9aa31c570923114d14
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-03-27 23:35:36 -0300

    developing home

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,115 @@
+// main.dart
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Avaliacao());
+  runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: const Portfolio()),
+  );
 }
 
-class Avaliacao extends StatelessWidget {
-  const Avaliacao({super.key});
+class Portfolio extends StatelessWidget {
+  const Portfolio({super.key});
+
+  static String routeName = 'Home';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Portfolio",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Inicio(),
-    );
-  }
-}
+    final textTheme = Theme.of(context).textTheme;
 
-class Inicio extends StatelessWidget {
-  const Inicio({super.key});
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(333),
+                  child: Image.asset(
+                    "assets/foto.webp",
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF5F6FA),
-      appBar: AppBar(
-        title: Text("Portfolio", style: TextStyle(color: Colors.white)),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
+                Text(
+                  "Kauan Vinícius",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
 
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Texto de exemplo")
-            ]
+                Text(
+                  "Software - Data Science e Logística | Bosch",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 16,
+                    color: Colors.grey[600],
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.all(6),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _btn(
+                        text: "Ver projetos",
+                        onTap: () {},
+                        color: const Color.fromARGB(255, 41, 117, 179),
+                        textColor: Colors.white,
+                      ),
+                      SizedBox(width: 12),
+                      _btn(
+                        text: "Contato",
+                        onTap: () {},
+                        color: Colors.white,
+                        textColor: const Color.fromARGB(255, 41, 117, 179),
+                        border: true,
+                      ),
+                    ],
+                  ),
+                ),
+
+                Text(
+                  "SOBRE",
+                  style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.all(18),
+                  child: Text(
+                    "Meu nome é Kauan Vinícius, tenho 17 anos, nascido em Itapira no interior do estado de São Paulo e morando atualmente em Campinas. Atuo na empresa Robert Bosch como Jovem Aprendiz, cursando o terceiro semestre de Desenvolvimento de Sistemas no SENAI Roberto Mange e sou estudante do 3º ano da Escola Estadual Dom Barreto no período noturno.\n\nPrezo pelo desenvolvimento de código limpo, organização, planejamento e prototipação de projetos. Sou comunicativo, trabalho bem em equipe e possuo inglês intermediário, estudando atualmente na CPM (Cidadão Pro Mundo) em parceria com a Universidade Presbiteriana Mackenzie",
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  // Molde de um botão personalizado
+  Widget _btn({
+    required String text,
+    required VoidCallback onTap,
+    required Color color,
+    required Color textColor,
+    bool border = false,
+  }) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(backgroundColor: color),
+      child: Text(text),
     );
   }
 }
